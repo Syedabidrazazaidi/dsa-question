cd98ddd24bd475246f9a999330015d11cd7db20b
Syedabidrazazaidi <zaidiabid246@gmail.com>
2026-04-26 13:59:16 +0530

    Update two-sum.java

--- array/two-sum.java
+++ array/two-sum.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int n=nums.length;
+       int ans[]={-1};
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int partner=target-nums[i];
+            if(mp.containsKey(partner)){
+               ans= new int[]{mp.get(partner),i};
+            }
+             mp.put(nums[i],i);
+        }
+             return ans;
+    }
+}
